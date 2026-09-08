93fc8fe980f27bd7ccf5307ffb4f441e961f616c
ajigocollins-creator <ajigocollins@gmail.com>
2026-09-08 02:12:49 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,9 +2,11 @@
 // SUPABASE CONFIG
 // ============================================
 
-const SUPABASE_URL = 'https://tbdhphqtttqbnpvfaijo.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://tbdhphqtttqbnpvfaijo.supabase.co';
+
 const SUPABASE_ANON_KEY = 'sb_publishable_eb_2A9Wa94i7gv9FhFPNxA_D3dZkDNL';
 
 const USE_SUPABASE = Boolean(
   SUPABASE_URL && SUPABASE_ANON_KEY
 );
+
