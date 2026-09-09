c08a2914b1f719c1cc8543459f0e097806adeba7
ajigocollins-creator <ajigocollins@gmail.com>
2026-09-09 14:11:26 +0100

    Update SUPABASE_URL to remove endpoint path

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
 // SUPABASE CONFIG
 // ============================================
 
-const SUPABASE_URL = 'https://tbdhphqtttqbnpvfaijo.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://tbdhphqtttqbnpvfaijo.supabase.co';
 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiZGhwaHF0dHRxYm5wdmZhaWpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2MTgwNDMsImV4cCI6MjEwNDE5NDA0M30.0Eyy7hKhLjdt_qWu9B5E1sVutHXYvhr7frOul6wgXi8';
 
