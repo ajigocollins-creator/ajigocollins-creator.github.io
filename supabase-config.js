1156ebe41e9d79461582763e420553cec3d3c1af
ajigocollins-creator <ajigocollins@gmail.com>
2026-09-09 14:01:59 +0100

    Update Supabase URL and ANON key

--- supabase-config.js
+++ supabase-config.js
@@ -2,9 +2,9 @@
 // SUPABASE CONFIG
 // ============================================
 
-const SUPABASE_URL = 'https://tbdhphqtttqbnpvfaijo.supabase.co';
+const SUPABASE_URL = 'https://tbdhphqtttqbnpvfaijo.supabase.co/rest/v1/';
 
-const SUPABASE_ANON_KEY = 'sb_publishable_eb_2A9Wa94i7gv9FhFPNxA_D3dZkDNL';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiZGhwaHF0dHRxYm5wdmZhaWpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2MTgwNDMsImV4cCI6MjEwNDE5NDA0M30.0Eyy7hKhLjdt_qWu9B5E1sVutHXYvhr7frOul6wgXi8';
 
 const USE_SUPABASE = Boolean(
   SUPABASE_URL && SUPABASE_ANON_KEY
