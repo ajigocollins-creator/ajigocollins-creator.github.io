e84b7a210c549758af03e027513b346c7908ea74
ajigocollins-creator <ajigocollins@gmail.com>
2026-09-15 13:56:18 +0100

    Update products.js

--- products.js
+++ products.js
@@ -106,7 +106,7 @@ async function loadProductsFromSupabase() {
         id: product.id,
         name: product.name || "Unnamed Product",
         price: Number(product.price) || 0,
-        image: product.image || product.image_url || "logo.png",
+        image: product.image_url || product.image || "logo.png",
         available: product.available !== false
       };
     });
